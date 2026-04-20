c4c28969f2f7147fa68381b622049fc3d28be1fd
bijjamvijayreddy <bijjamvijayreddy@gmail.com>
2026-04-20 15:43:30 +0530

    remove the unwanted deleted files

--- frontend/dashboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protect } from "../middleware/auth.middleware";
-import { getOverview } from "../controllers/dashboard.controller";
-
-const router = Router();
-
-router.get("/overview", protect, getOverview);
-
-export default router;
